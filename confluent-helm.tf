100710ca9df329c1a9a16c715732ee1c4452de79
Joshua Hoblitt <josh@hoblitt.com>
2018-11-20 17:50:28 -0700

    add "load-balancer" data resources

---
+++ confluent-helm.tf
@@ -0,0 +1,47 @@
+resource "helm_repository" "confluentinc" {
+  name = "confluentinc"
+  url  = "https://raw.githubusercontent.com/lsst-sqre/cp-helm-charts/master"
+}
+
+resource "helm_release" "confluent" {
+  name       = "confluent"
+  repository = "${helm_repository.confluentinc.metadata.0.name}"
+  chart      = "cp-helm-charts"
+  namespace  = "${data.template_file.k8s_namespace.rendered}"
+
+  force_update  = true
+  recreate_pods = true
+}
+
+data "kubernetes_service" "lb0" {
+  metadata {
+    name = "confluent-0-loadbalancer"
+    namespace = "${data.template_file.k8s_namespace.rendered}"
+  }
+}
+
+data "kubernetes_service" "lb1" {
+  metadata {
+    name = "confluent-1-loadbalancer"
+    namespace = "${data.template_file.k8s_namespace.rendered}"
+  }
+}
+
+data "kubernetes_service" "lb2" {
+  metadata {
+    name = "confluent-2-loadbalancer"
+    namespace = "${data.template_file.k8s_namespace.rendered}"
+  }
+}
+
+data "template_file" "lb0_ip" {
+  template = "${lookup(data.kubernetes_service.lb0.load_balancer_ingress[0], "ip")}"
+}
+
+data "template_file" "lb1_ip" {
+  template = "${lookup(data.kubernetes_service.lb1.load_balancer_ingress[0], "ip")}"
+}
+
+data "template_file" "lb2_ip" {
+  template = "${lookup(data.kubernetes_service.lb2.load_balancer_ingress[0], "ip")}"
+}
---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "lb0" {
+  value = "${data.template_file.lb0_ip.rendered}"
+}
+
+output "lb1" {
+  value = "${data.template_file.lb1_ip.rendered}"
+}
+
+output "lb2" {
+  value = "${data.template_file.lb2_ip.rendered}"
+}
