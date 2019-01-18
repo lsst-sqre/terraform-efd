232eb21c19a2ce99d67896ca374f2daf091fff03
Joshua Hoblitt <josh@hoblitt.com>
2019-01-18 14:46:33 -0700

    bump helm provider to ~> 0.7.0

--- main.tf
+++ main.tf
@@ -47,11 +47,11 @@ module "tiller" {
   source          = "git::https://github.com/lsst-sqre/terraform-tinfoil-tiller.git//?ref=master"
   namespace       = "kube-system"
   service_account = "tiller"
-  tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.9.0"
+  tiller_image    = "gcr.io/kubernetes-helm/tiller:v2.11.0"
 }
 
 provider "helm" {
-  version = "~> 0.6.2"
+  version = "~> 0.7.0"
 
   service_account = "${module.tiller.service_account}"
   namespace       = "${module.tiller.namespace}"
