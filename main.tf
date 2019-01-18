697ff326515b2bc2427e25ddb24d41352f275371
Joshua Hoblitt <josh@hoblitt.com>
2019-01-18 14:38:37 -0700

    update gke-std mod vars

--- main.tf
+++ main.tf
@@ -3,11 +3,12 @@ provider "template" {
 }
 
 module "gke" {
-  source         = "git::https://github.com/lsst-sqre/terraform-gke-std.git//?ref=master"
-  name           = "${local.gke_cluster_name}"
-  google_project = "${var.google_project}"
-  gke_version    = "latest"
-  machine_type   = "n1-standard-2"
+  source             = "git::https://github.com/lsst-sqre/terraform-gke-std.git//?ref=master"
+  name               = "${local.gke_cluster_name}"
+  google_project     = "${var.google_project}"
+  gke_version        = "latest"
+  initial_node_count = 3
+  machine_type       = "n1-standard-2"
 }
 
 provider "kubernetes" {
